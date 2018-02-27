eb3b50d37b144c079cc41b423e34696c0114e45c
Stanislavs Barkovskis <stanislavs.barkovskis@hotmail.com>
2018-02-27 14:57:36 +0200

    Basktet-dirty

--- ECommerce/ECommerceDomain/Basket/Basket.cs
+++ ECommerce/ECommerceDomain/Basket/Basket.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ECommerceDomain.Basket
 {
-    class Basket
+    public class Basket
     {
+        private readonly IPrincipal owner;
+        private readonly List<Extent> contents;
+
+        public Basket(IPrincipal owner)
+        {
+            if (owner == null)
+                throw new ArgumentNullException("owner");
+            this.contents = new List<Extent>();
+
+            this.owner = owner;
+        }
+
+        public IList<Extent>Contents
+        { get { return this.contents; } }
+
+        public IPrincipal Owner { get { return this.owner; } }
+
+        
     }
 }
--- ECommerce/ECommerceDomain/Basket/Extent.cs
+++ ECommerce/ECommerceDomain/Basket/Extent.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ECommerceDomain.Basket
 {
-    class Extent
+    public class Extent
     {
+        private readonly Product product;
+
+        public Extent(Product product)
+        {
+            if (product == null)
+                throw new ArgumentNullException("product");
+
+            this.product = product;
+        }
+
+        public Product Product { get { return this.product; } }
+
+        public int Quantity { get; set; }
+
     }
 }
